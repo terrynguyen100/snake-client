7a8ea41bd694be7830cf66a5d3a1262a6cbdcb06
Terry Nguyen <terrynguyen100@gmail.com>
2023-05-10 20:53:57 -0600

    add an input system

--- play.js
+++ play.js
@@ -7,8 +7,28 @@ const conn = connect();
 conn.on('connect', () => {
   console.log('Successfully connected to game server!');
   conn.write('Name: TER');
+  //conn.write('Move: up');
 });
 
+// setup interface to handle user input from stdin
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+// \u0003 maps to ctrl+c input
+  if (key === '\u0003') {
+    process.exit();
+}
+};
+
+
+setupInput();
 // listening for an show message from the server
 conn.on('data', (data) => {
   console.log(data);
