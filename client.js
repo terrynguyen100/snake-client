7428fbc3bf53820c5be94206e9280ce841550ba7
Terry Nguyen <terrynguyen100@gmail.com>
2023-05-10 21:33:58 -0600

    add constants.js to store IP and PORT

--- client.js
+++ client.js
@@ -1,11 +1,11 @@
 const net = require('net');
-
+const {IP, PORT} = require('./constants');
 //establish a connection with a game server
 
 const connect = function () {
   const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: '50541'
+    host: IP,
+    port: PORT
   });
 
   //interpret incoming data as text
