f3257962b7b196172ee4459d44bfec5e957d1753
Terry Nguyen <terrynguyen100@gmail.com>
2023-05-10 20:37:44 -0600

    add a initial, send message to server when connected

--- play.js
+++ play.js
@@ -5,7 +5,8 @@ const conn = connect();
 
 //for when the connection is first made
 conn.on('connect', () => {
-  console.log('Connected!');
+  console.log('Successfully connected to game server!');
+  conn.write('Name: TER');
 });
 
 // listening for an show message from the server
